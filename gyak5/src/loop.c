82acaa7f9d8eca26ebf5063ede98f951348fdd89
sodamachinebroke <erseknorbi2000@gmail.com>
2023-04-06 09:40:36 +0200

    started the line drawing program

--- gyak5/src/loop.c
+++ gyak5/src/loop.c
@@ -1,4 +1,11 @@
 #include "loop.h"
+#include <stdbool.h>
+
+void drawLine(SDL_Renderer *renderer, int x1, int x2, int y1, int y2)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+}
 
 void loopThis()
 {
@@ -17,6 +24,8 @@ void loopThis()
     screen = SDL_CreateWindow("Draw me some gay stuff", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
     renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
 
+    int x1, x2, y1, y2;
+
     int run = 1;
     while (run == 1)
     {
@@ -29,8 +38,20 @@ void loopThis()
             case SDL_MOUSEBUTTONDOWN:
                 if (event.button.button == SDL_BUTTON_LEFT)
                 {
-                    chatGPTGayRectGen(renderer, event);
+                    x1 = event.button.x;
+                    y1 = event.button.y;
                 }
+
+                break;
+
+            case SDL_MOUSEBUTTONUP:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    x2 = event.button.x;
+                    y2 = event.button.y;
+
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+                SDL_RenderPresent(renderer);
                 break;
             case SDL_SCANCODE_Q:
                 run = 0;
